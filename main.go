3d4ac69c86ac73a62a57be5e51087c3b5c3aec31
Omoba <ogabiprince@gmail.com>
2024-03-16 09:38:03 +0100

    removed service.go file

--- main.go
+++ main.go
@@ -9,7 +9,17 @@ import (
 	"gogcp/resources"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/api/compute/v1"
 )
+
+func newComputeService(ctx context.Context) (*compute.Service, error) {
+	// Create a new Compute Engine service client
+	service, err := compute.NewService(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create service: %v", err)
+	}
+	return service, nil
+}
 
 func main() {
 	// load .env file
@@ -20,30 +30,30 @@ func main() {
 	}
 
 	ctx := context.Background()
-	service, _ := NewComputeService(ctx)
+	service, _ := newComputeService(ctx)
 
 	projectID := os.Getenv("projectID")
 	instanceNameTemplate := "test-instance-%d"
 	zone := "us-central1-a"
-	machineType := "e2-small"
-
-	//We can use go routines to create instances in parallel
-	for i := 0; i < 2; i++ {
-		instanceName := fmt.Sprintf(instanceNameTemplate, i)
-		err := resources.CreateInstance(service, projectID, instanceName, zone, machineType)
-		if err != nil {
-			log.Fatalf("Failed to create instance: %v", err)
-		}
-	}
+	// machineType := "e2-small"
 
 	// //We can use go routines to create instances in parallel
 	// for i := 0; i < 2; i++ {
 	// 	instanceName := fmt.Sprintf(instanceNameTemplate, i)
-	// 	err := resources.DeleteInstance(service, projectID, instanceName, zone)
+	// 	err := resources.CreateInstance(service, projectID, instanceName, zone, machineType)
 	// 	if err != nil {
-	// 		log.Fatalf("Failed to delete instance: %v", err)
+	// 		log.Fatalf("Failed to create instance: %v", err)
 	// 	}
 	// }
+
+	//We can use go routines to create instances in parallel
+	for i := 0; i < 2; i++ {
+		instanceName := fmt.Sprintf(instanceNameTemplate, i)
+		err := resources.DeleteInstance(service, projectID, instanceName, zone)
+		if err != nil {
+			log.Fatalf("Failed to delete instance: %v", err)
+		}
+	}
 
 	resources.SomeMultiplier(23)
 }
